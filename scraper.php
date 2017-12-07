e00ffe25f96a49cf079615bc2218c0ea178eefe8
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-12-07 11:19:54 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -8,7 +8,7 @@ require 'scraperwiki/simple_html_dom.php';
 // // Read in a page
 // $html = scraperwiki::scrape("http://foo.com");
 	for($PageLoop = 100; $PageLoop < 103; $PageLoop++){
-$html = scraperwiki::scrape("ww3.comsats.edu.pk/alumni/SearchDetailsOutside.aspx?Id=".$PageLoop);
+$html = file_get_html("ww3.comsats.edu.pk/alumni/SearchDetailsOutside.aspx?Id=".$PageLoop);
 echo $html;
 }
 //$name  =      $html->find('span[id="ctl00_ContentPlaceHolder1_DetailsView2_Label11"]',0)->plaintext;
