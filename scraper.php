c9c43891a5210c81c59b865b035f696cbeaec06b
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-23 16:01:38 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,33 +1,12 @@
 <?
-// This is a template for a PHP scraper on morph.io (https://morph.io)
-// including some code snippets below that you should find helpful
-
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-	for($PageLoop = 100; $PageLoop < 103; $PageLoop++){
-$html = file_get_html("ww3.comsats.edu.pk/alumni/SearchDetailsOutside.aspx?Id=".$PageLoop);
-echo $html;
-}
-//$name  =      $html->find('span[id="ctl00_ContentPlaceHolder1_DetailsView2_Label11"]',0)->plaintext;
-//echo $name;
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
 
-// You don't have to do things with the ScraperWiki library.
-// You can use whatever libraries you want: https://morph.io/documentation/php
-// All that matters is that your final data is written to an SQLite database
-// called "data.sqlite" in the current working directory which has at least a table
-// called "data".
+	for($PageLoop = 100; $PageLoop < 103; $PageLoop++)
+	{
+	$html 	= file_get_html("ww3.comsats.edu.pk/alumni/SearchDetailsOutside.aspx?Id=".$PageLoop);
+	$name  	=      $html->find('span[id="ctl00_ContentPlaceHolder1_DetailsView2_Label11"]',0)->plaintext;
+	echo "$name\n";
+	}
+
 ?>
